4ff65d4f0ce65f310ed69f375899ce5b66c19a23
Henry Wong <henryw48@nycstudents.net>
2022-10-05 12:29:34 -0400

    added code

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TipCalculator {
-}
+import java.util.*;
+    public class TipCalculator {
+        public static void main(String[] args) {
+            Scanner bill = new Scanner(System.in);
+            Scanner tip = new Scanner(System.in);
+            Scanner person = new Scanner(System.in);
+
+            System.out.println("Bill :");
+            double b = bill.nextDouble();
+            System.out.println("Tip % : ");
+            double t = tip.nextDouble() / 100;
+            System.out.println("Number of People : ");
+            int n = person.nextInt();
+
+            double totaltip = b * t;
+            double tipperperson = totaltip / n;
+            double totalbill = b + totaltip;
+            double totalbillperperson = totalbill / n;
+
+            System.out.println("The total tip amount is: $ "+totaltip);
+            System.out.println("The total bill including tip is: $ "+totalbill);
+            System.out.println("The tip per person is: $ "+tipperperson);
+            System.out.println("The total bill per person is: $ "+totalbillperperson);
+        }
+    }
